58b79c5646038ce6c66154c508ed3c5224d1acc8
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2021-01-30 12:19:40 +0100

    feat: force h3 size ; Delete descriptions ; Add complete check ; Add colors to icons ; Include interaction key

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -9,67 +9,73 @@ export const Timeline = () => {
   const milestones = [
     {
       header: 'Pagar',
-      description: 'La parte mas importante',
       status: 'completed',
     },
     {
       header: 'El sistema financiero',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Análisis técnico',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Tipos de inversores',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Análisis fundamental y macroeconómico',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Gestión monetaria',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Gestión del riesgo',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Consideraciones finales',
-      description: 'La parte mas importante',
       status: 'notstarted',
     },
     {
       header: 'Bitcoin',
       status: 'notstarted',
-      icon: mdiBitcoin,
+      icon: {
+        path: mdiBitcoin,
+        color: '#f90',
+      },
     },
   ];
 
   const getMedia = (milestone) => {
     let media;
     if (milestone.icon) {
-      media = <Icon size={0.9} path={milestone.icon} />;
+      media = (
+        <Icon
+          size={0.9}
+          path={milestone.icon.path}
+          color={milestone.icon.color}
+        />
+      );
     } else if (milestone.status === 'inprogress') {
       media = <Spinner animation="grow" variant="primary" />;
     } else if (milestone.status === 'completed') {
-      media = <Icon size={0.9} path={mdiCheckCircle} />;
+      media = <Icon size={0.9} path={mdiCheckCircle} color="green" />;
     }
     return media;
   };
 
   return (
     <ul className="timeline">
-      {milestones.map((milestone) => (
-        <li className="event" completed={milestone.completed}>
+      {milestones.map((milestone, index) => (
+        <li
+          key={index}
+          className={['event', milestone.status === 'completed' && 'completed']
+            .filter((a) => a)
+            .join(' ')}
+        >
           <div className="media">{getMedia(milestone)}</div>
           <div className="text">
             <h3 className="h3">{milestone.header}</h3>
